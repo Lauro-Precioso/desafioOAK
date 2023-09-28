4e3f9d5d2c53cb429d4e123633c2dbeab94d179c
lauro-precioso <lauroprecioso99@gmail.com>
2023-09-28 19:16:07 -0300

    correção de erros

--- produtos.py
+++ produtos.py
@@ -1,8 +1,10 @@
 import tkinter as tk
-from tkinter import messagebox, ttk
+from tkinter import ttk, messagebox
 
+# Lista de produtos
 produtos = []
 
+# Função para cadastrar um novo produto
 def cadastrar_produto():
     nome = entry_nome.get()
     descricao = entry_descricao.get()
@@ -10,37 +12,43 @@ def cadastrar_produto():
     disponivel = var_disponivel.get()
 
     if nome and valor:
-        produtos.append({"Nome": nome, "Descrição": descricao, "Valor": valor, "Disponível": disponivel})
+        # Substitui a vírgula por ponto para garantir que seja um valor válido em ponto flutuante
+        valor = valor.replace(",", ".")
+
+        produtos.append({"Nome": nome, "Descrição": descricao, "Valor": float(valor), "Disponível": disponivel})
         limpar_campos()
         atualizar_listagem()
     else:
         messagebox.showerror("Erro", "Por favor, preencha o nome e o valor do produto.")
 
+# Função para limpar os campos do formulário
 def limpar_campos():
     entry_nome.delete(0, tk.END)
     entry_descricao.delete(0, tk.END)
     entry_valor.delete(0, tk.END)
     var_disponivel.set(0)
 
+# Função para atualizar a listagem de produtos
 def atualizar_listagem():
     listbox.delete(0, tk.END)
     produtos.sort(key=lambda x: x["Valor"])
     for produto in produtos:
-        listbox.insert(tk.END, f"{produto['Nome']} - R${produto['Valor']:.2f}")
-
-def cadastrar_e_listar():
-    cadastrar_produto()
-    aba_control.hide(aba_cadastro)
-    aba_control.select(aba_listagem)
+        listbox.insert(tk.END, "{} - R${:.2f}".format(produto['Nome'], produto['Valor']))
 
 # Configuração da janela principal
 root = tk.Tk()
 root.title("Cadastro e Listagem de Produtos")
+root.geometry("400x250")
 
-# Janela de cadastro
-aba_cadastro = tk.Frame(root)
-aba_cadastro.pack(fill="both", expand=True)
+# Abas
+aba_control = ttk.Notebook(root)
+aba_cadastro = ttk.Frame(aba_control)
+aba_listagem = ttk.Frame(aba_control)
+aba_control.add(aba_cadastro, text="Cadastro")
+aba_control.add(aba_listagem, text="Listagem")
+aba_control.pack(fill="both", expand=True)
 
+# Formulário de cadastro
 label_nome = tk.Label(aba_cadastro, text="Nome do Produto:")
 label_nome.pack()
 entry_nome = tk.Entry(aba_cadastro)
@@ -64,22 +72,14 @@ radio_nao = tk.Radiobutton(aba_cadastro, text="Não", variable=var_disponivel, v
 radio_sim.pack()
 radio_nao.pack()
 
-botao_cadastrar = tk.Button(aba_cadastro, text="Cadastrar", command=cadastrar_e_listar)
+botao_cadastrar = tk.Button(aba_cadastro, text="Cadastrar", command=cadastrar_produto)
 botao_cadastrar.pack()
 
-# Janela de listagem
-aba_listagem = tk.Frame(root)
-aba_listagem.pack(fill="both", expand=True)
-
+# Listagem de produtos
 listbox = tk.Listbox(aba_listagem)
 listbox.pack(fill="both", expand=True)
 
-botao_novo = tk.Button(aba_listagem, text="Novo Produto", command=lambda: aba_control.forget(aba_listagem) or aba_control.select(aba_cadastro))
+botao_novo = tk.Button(aba_listagem, text="Novo Produto", command=lambda: aba_control.select(aba_cadastro))
 botao_novo.pack()
-
-aba_control = ttk.Notebook(root)
-aba_control.add(aba_cadastro, text="Cadastro")
-aba_control.add(aba_listagem, text="Listagem")
-aba_control.pack(fill="both", expand=True)
 
 root.mainloop()
